c20468b19cfb9cfac912712db111b6cfc43239f4
Alok Bansode <alokdeeps@Aloks-MacBook-Pro.local>
2018-08-18 14:12:13 +0100

    First commit

---
+++ nightwatch.conf.js
@@ -0,0 +1,47 @@
+const seleniumServer = require('selenium-server');
+const geckodriver = require('geckodriver');
+
+require('nightwatch-cucumber')({
+  cucumberArgs: [
+    '--require', 'step_definitions',
+    '--format', 'node_modules/cucumber-pretty',
+    '--format', 'json:reports/cucumber.json',
+    'features']
+});
+
+module.exports = {
+  output_folder: 'reports',
+  custom_assertions_path: '',
+  live_output: false,
+  disable_colors: false,
+  selenium: {
+    start_process: true,
+    server_path: seleniumServer.path,
+    log_path: '',
+    host: '127.0.0.1',
+    port: 4444
+  },
+  test_settings: {
+    default: {
+      launch_url: 'https://datagardener.com/#/pages/homePage',
+      selenium_port: 4444,
+      selenium_host: '127.0.0.1',
+      desiredCapabilities: {
+        browserName: 'firefox',
+        javascriptEnabled: true
+      }
+    },
+    firefox: {
+      desiredCapabilities: {
+        browserName: 'firefox',
+        javascriptEnabled: true,
+        acceptSslCerts: true
+      },
+      selenium: {
+        cli_args: {
+          'webdriver.geckodriver.driver': geckodriver.path
+        }
+      }
+    }
+  }
+};
---
+++ step_definitions/googleSearchSteps.js
@@ -0,0 +1,22 @@
+const { client, expect } = require('nightwatch-cucumber');
+const { Given, Then, When } = require('cucumber');
+const sleep = require('thread-sleep');
+
+
+Given('I am on DataGardener Website', function () {
+    return client.url(client.launch_url).waitForElementVisible('body', 20000);
+});
+
+Then('I should be able to see home page with {string}', function (message) {
+    return client.expect.element('.text-uppercase > strong:nth-child(1)').text.to.contain(message);
+  });
+
+When('I search for a company {string}', function (companyName) {
+      return client.waitForElementVisible('.company_search', 20000)
+      .setValue('.company_search', companyName).click('.btn-get-started');
+});
+
+Then('I should be able to see results',async function () {
+    await sleep(5000);
+    return client.expect.element('#table-test').to.be.visible;
+});
